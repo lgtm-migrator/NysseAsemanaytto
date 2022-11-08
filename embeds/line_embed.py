29651f50356d23c2beec07955645e8b26492a386
NAL <niko.a.leinonen@gmail.com>
2022-11-08 20:31:16 +0200

    Point calculations

--- embeds/line_embed.py
+++ embeds/line_embed.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 import datetime
+from typing import Sequence
 
 import embeds
 import digitransit.routing
 import pygame
+import pyproj
 
 from core import render_info, logging, config
+from nalpy import math
 import digitransit.routing
 
 class LineEmbed(embeds.Embed):
@@ -60,7 +63,30 @@ last_render_cache_clear: datetime.datetime | None = None
 line_render_cache_size: tuple[int, int] | None = None
 line_render_cache: dict[str, pygame.Surface]
 
+def _remapPoints(unscaled_points: Sequence[math.Vector2], size: tuple[float, float], padding: float) -> list[math.Vector2]:
+    unscaled_x: list[float] = [p.x for p in unscaled_points]
+    unscaled_y: list[float] = [p.y for p in unscaled_points]
+
+    left = min(unscaled_x)
+    right = max(unscaled_x)
+    top = max(unscaled_y) # up is positive for y
+    bottom = min(unscaled_y)
+
+    target_left: float = padding
+    target_right: float = size[0] - padding
+    target_top: float = padding # down is positive for y
+    target_bottom: float = size[1] - padding
+
+    def remapper(point: math.Vector2):
+        x = math.remap(point.x, left, right, target_left, target_right)
+        y = math.remap(point.y, top, bottom, target_top, target_bottom)
+        return math.Vector2(x, y)
+
+    return [remapper(p) for p in unscaled_points]
+
 def render_line_for_pattern(patternCode: str, size: tuple[int, int]) -> pygame.Surface | None:
+    PADDING = size[1] / 10
+
     pattern_geometry: digitransit.routing.PatternGeometry
     try:
         pattern_geometry = digitransit.routing.get_pattern_geometry(config.current.endpoint, patternCode)
@@ -68,4 +94,16 @@ def render_line_for_pattern(patternCode: str, size: tuple[int, int]) -> pygame.S
         logging.dump_exception(e, note="lineEmbed")
         return None
 
+    unscaled_points: list[math.Vector2] = [_projectCoordinates(coordinate) for coordinate in pattern_geometry.geometry]
+    points: list[math.Vector2] = _remapPoints(unscaled_points, size, PADDING)
 
+    surface: pygame.Surface = pygame.Surface(size)
+    surface.fill((255, 255, 255))
+    raise NotImplementedError()
+    pygame.draw.lines(surface, ())
+
+
+lonlat_to_webmercator: pyproj.Transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
+def _projectCoordinates(coordinate: digitransit.routing.Coordinate) -> math.Vector2:
+    x, y = lonlat_to_webmercator.transform(coordinate.longitude, coordinate.latitude)
+    return math.Vector2(x, y)
